f778e6b73fa38aee8dcde35f4d696924df7f5d41
鎌田由美子 <feg.yu1892@gmail.com>
2025-09-13 09:23:13 +0900

    詳細ページ確認 → 投稿フォーム実装

--- app/controllers/diary_entries_controller.rb
+++ app/controllers/diary_entries_controller.rb
@@ -5,7 +5,26 @@ class DiaryEntriesController < ApplicationController
     @entries = current_user.diary_entries.order(walked_on: :desc)
   end
 
+  def new
+    @entry = current_user.diary_entries.new
+  end
+
+  def create
+    @entry = current_user.diary_entries.new(entry_params)
+    if @entry.save
+      redirect_to diary_entries_path(@entry), notice: "日記を投稿しました"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def show
     @entry = current_user.diary_entries.find(params[:id])
   end
+end
+
+private
+
+def entry_params
+  params.require(:diary_entries).permit(:body, :mood, :walked_on)
 end
--- app/models/diary_entry.rb
+++ app/models/diary_entry.rb
@@ -1,3 +1,11 @@
 class DiaryEntry < ApplicationRecord
   belongs_to :user
+
+  enum mood: {
+    good: 0,
+    normal: 1,
+    bad: 2,
+    excited: 3,
+    tired: 4
+  }
 end
