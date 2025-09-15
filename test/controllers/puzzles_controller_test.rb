5b454c7ce091e1e0f50fb62a74c3ccba17a0fc75
鎌田由美子 <feg.yu1892@gmail.com>
2025-09-15 22:08:00 +0900

    CIのtest修正

--- test/controllers/puzzles_controller_test.rb
+++ test/controllers/puzzles_controller_test.rb
@@ -2,12 +2,12 @@ require "test_helper"
 
 class PuzzlesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get puzzles_index_url
+    get puzzles_url
     assert_response :success
   end
 
   test "should get show" do
-    get puzzles_show_url
+    get puzzle_url(puzzles(:one))
     assert_response :success
   end
 end
